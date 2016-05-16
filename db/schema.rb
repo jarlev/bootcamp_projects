75fc443d83ccef3590f6284301560ddce9bf530a
Jared Levin <jaredrlevin@gmail.com>
2016-05-16 09:39:35 -0700

    added username to user model, username to devise registration views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516161549) do
+ActiveRecord::Schema.define(version: 20160516163400) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "description"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160516161549) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
